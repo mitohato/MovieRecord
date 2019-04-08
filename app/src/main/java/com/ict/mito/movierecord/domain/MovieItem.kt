8d54a10b048f682b2ef89671ac246bf99e00010f
mitohato <mitohato14@gmail.com>
2019-04-08 21:42:06 +0900

    Add id content

--- app/src/main/java/com/ict/mito/movierecord/domain/MovieItem.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/MovieItem.kt
@@ -4,6 +4,7 @@ package com.ict.mito.movierecord.domain
  * Created by mitohato14 on 2019/04/08.
  */
 data class MovieItem(
+    val movieId: String,
     val title: String,
     val description: String,
     val imageUrl: String
