e59494b7dc280206bf4bb07d77dfa14809d2a7ac
mitohato <mitohato14@gmail.com>
2019-04-08 21:40:20 +0900

    Add movie item

---
+++ app/src/main/java/com/ict/mito/movierecord/domain/MovieItem.kt
@@ -0,0 +1,10 @@
+package com.ict.mito.movierecord.domain
+
+/**
+ * Created by mitohato14 on 2019/04/08.
+ */
+data class MovieItem(
+    val title: String,
+    val description: String,
+    val imageUrl: String
+)
