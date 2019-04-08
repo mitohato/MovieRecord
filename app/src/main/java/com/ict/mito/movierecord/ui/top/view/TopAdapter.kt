381ad48c760a016d7f6bdb8195adc7fe7ea79a55
mitohato <mitohato14@gmail.com>
2019-04-08 21:49:51 +0900

    Add adapter and viewholder class

--- app/src/main/java/com/ict/mito/movierecord/ui/top/view/TopAdapter.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/top/view/TopAdapter.kt
@@ -1,7 +1,27 @@
 package com.ict.mito.movierecord.ui.top.view
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ict.mito.movierecord.databinding.MovieCardContentBinding
+import com.ict.mito.movierecord.domain.MovieItem
+
 /**
  * Created by mitohato14 on 2019/04/07.
  */
-class TopAdapter {
+class TopAdapter(private val movieList: List<MovieItem>) : RecyclerView.Adapter<TopAdapter.TopViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: TopViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class TopViewHolder(private val binding: MovieCardContentBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
 }
