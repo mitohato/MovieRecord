a91eff0b254ac1b6e68f80b8a87bc8831d8f8761
mitohato <mitohato14@gmail.com>
2019-04-10 11:12:12 +0900

    Add memb value

--- app/src/main/java/com/ict/mito/movierecord/ui/top/TopViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/top/TopViewModel.kt
@@ -1,7 +1,12 @@
 package com.ict.mito.movierecord.ui.top
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ict.mito.movierecord.domain.MovieItem
+import com.ict.mito.movierecord.ui.top.view.TopAdapter
 
 class TopViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var movieListLiveData: LiveData<List<MovieItem>> = MutableLiveData()
+    val adapter: TopAdapter = TopAdapter(movieListLiveData.value ?: listOf())
 }
