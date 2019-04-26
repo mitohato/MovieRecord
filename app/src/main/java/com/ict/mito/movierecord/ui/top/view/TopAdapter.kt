a0c898eae3dd010ba610cccc2e220eb7cd2b4536
mitohato <mitohato14@gmail.com>
2019-04-27 06:10:56 +0900

    Devide Adapter and ViewHolder

--- app/src/main/java/com/ict/mito/movierecord/ui/top/view/TopAdapter.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/top/view/TopAdapter.kt
@@ -1,16 +1,14 @@
 package com.ict.mito.movierecord.ui.top.view
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.ict.mito.movierecord.databinding.MovieCardBinding
 import com.ict.mito.movierecord.domain.MovieItem
 
 /**
  * Created by mitohato14 on 2019/04/07.
  */
-class TopAdapter(private var movieList: List<MovieItem>) : RecyclerView.Adapter<TopAdapter.TopViewHolder>() {
+class TopAdapter(private var movieList: List<MovieItem>) : RecyclerView.Adapter<TopViewHolder>() {
 
     fun setMovieItemList(movieItemList: List<MovieItem>) {
         movieList = movieItemList
@@ -33,36 +31,5 @@ class TopAdapter(private var movieList: List<MovieItem>) : RecyclerView.Adapter<
         position: Int
     ) {
         holder.bind(movieList[position])
-    }
-
-    class TopViewHolder(private val binding: MovieCardBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(movieItem: MovieItem) {
-            binding.also {
-                it.movieItem = movieItem
-                it.executePendingBindings()
-            }
-        }
-
-        fun unbind() {
-            binding.unbind()
-        }
-
-        fun setOnClickListener(onClickListener: View.OnClickListener) {
-            binding.root.setOnClickListener(onClickListener)
-        }
-
-        companion object {
-            fun create(
-                inflater: LayoutInflater,
-                parent: ViewGroup,
-                attachToRoot: Boolean
-            ): TopViewHolder = TopViewHolder(
-                MovieCardBinding.inflate(
-                    inflater,
-                    parent,
-                    attachToRoot
-                )
-            )
-        }
     }
 }
