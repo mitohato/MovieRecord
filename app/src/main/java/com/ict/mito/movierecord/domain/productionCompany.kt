27611301db0a80c2d3272687a2328747a2a11973
mitohato <mitohato14@gmail.com>
2019-06-19 16:25:46 +0900

    Add company data class

---
+++ app/src/main/java/com/ict/mito/movierecord/domain/productionCompany.kt
@@ -0,0 +1,11 @@
+package com.ict.mito.movierecord.domain
+
+/**
+ * Created by mitohato14 on 2019-06-19.
+ */
+data class productionCompany(
+    val name: String,
+    val id: Int,
+    val logo_path: String?,
+    val origin_country: String
+)
