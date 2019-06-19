81e8532956eb22a49e8e52826797b0615506b981
mitohato <mitohato14@gmail.com>
2019-06-19 16:26:46 +0900

    Rename to Camel

--- app/src/main/java/com/ict/mito/movierecord/domain/ProductionCompany.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/ProductionCompany.kt
@@ -3,7 +3,7 @@ package com.ict.mito.movierecord.domain
 /**
  * Created by mitohato14 on 2019-06-19.
  */
-data class productionCompany(
+data class ProductionCompany(
     val name: String,
     val id: Int,
     val logo_path: String?,
