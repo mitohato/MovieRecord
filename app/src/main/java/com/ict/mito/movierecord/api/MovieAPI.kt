77d089adadabc8d6fe7ce0dcf0baedf33bcf0670
mitohato <mitohato14@gmail.com>
2019-06-19 14:33:30 +0900

    Add path and query in get movie detail

--- app/src/main/java/com/ict/mito/movierecord/api/MovieAPI.kt
+++ app/src/main/java/com/ict/mito/movierecord/api/MovieAPI.kt
@@ -1,6 +1,7 @@
 package com.ict.mito.movierecord.api
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
@@ -8,7 +9,12 @@ import retrofit2.http.Query
  */
 interface MovieAPI {
 
-    fun getMovieDetail()
+    @GET("movie/{movie_id}")
+    fun getMovieDetail(
+        @Path("movie_id") id: Int,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "ja"
+    )
 
     @GET("movie/now_playing")
     fun getNowPlayingMovieList(
