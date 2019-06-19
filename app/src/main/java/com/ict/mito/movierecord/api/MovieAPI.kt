d805943a81bc462f2990878ff869d60ea7571738
mitohato <mitohato14@gmail.com>
2019-06-19 14:05:52 +0900

    Add get now playing movie list method

--- app/src/main/java/com/ict/mito/movierecord/api/MovieAPI.kt
+++ app/src/main/java/com/ict/mito/movierecord/api/MovieAPI.kt
@@ -1,5 +1,7 @@
 package com.ict.mito.movierecord.api
 
+import retrofit2.http.GET
+
 /**
  * Created by mitohato14 on 2019-04-21.
  */
@@ -8,4 +10,7 @@ interface MovieAPI {
     fun getMovieList()
 
     fun getMovieInfo()
+
+    @GET("movie/now_playing")
+    fun getNowPlayingMovieList()
 }
