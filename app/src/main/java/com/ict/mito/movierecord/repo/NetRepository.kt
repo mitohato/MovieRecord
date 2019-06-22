f51783309443c87116cb8f52610967da4460f5c5
mitohato <mitohato14@gmail.com>
2019-06-22 23:32:10 +0900

    Add http builder

--- app/src/main/java/com/ict/mito/movierecord/repo/NetRepository.kt
+++ app/src/main/java/com/ict/mito/movierecord/repo/NetRepository.kt
@@ -1,7 +1,30 @@
 package com.ict.mito.movierecord.repo
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
 /**
  * Created by mitohato14 on 2019-06-22.
  */
 class NetRepository {
+    val httpBuilder: OkHttpClient.Builder get() {
+        // create http client
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(Interceptor { chain ->
+                val original = chain.request()
+
+                //header
+                val request = original.newBuilder()
+                    .header("Accept", "application/json")
+                    .method(original.method(), original.body())
+                    .build()
+
+                return@Interceptor chain.proceed(request)
+            })
+            .readTimeout(30, TimeUnit.SECONDS)
+
+        return httpClient
+    }
+
 }
