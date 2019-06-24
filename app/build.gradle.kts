3ce4df97d37c1685910a2cc0c2d4f829c6871e9d
mitohato <mitohato14@gmail.com>
2019-06-24 22:24:46 +0900

    Add logger in okhttp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
     implementation("com.squareup.okhttp3:okhttp:3.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.10.0")
 
     //    room
     val roomVersion: String? by project
