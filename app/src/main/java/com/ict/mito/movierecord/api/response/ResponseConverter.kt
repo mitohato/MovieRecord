603aacef56ba63defe49bd65cf633be859de4f40
mitohato <mitohato14@gmail.com>
2019-06-28 11:46:25 +0900

    Change type and name id

--- app/src/main/java/com/ict/mito/movierecord/api/response/ResponseConverter.kt
+++ app/src/main/java/com/ict/mito/movierecord/api/response/ResponseConverter.kt
@@ -10,7 +10,7 @@ class ResponseConverter {
         return MovieItem(
             title = response.title,
             description = response.overview,
-            movieId = "${response.id}",
+            id = response.id,
             imageUrl = response.poster_path
         )
     }
--- app/src/main/java/com/ict/mito/movierecord/domain/DummyData.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/DummyData.kt
@@ -6,31 +6,31 @@ package com.ict.mito.movierecord.domain
 object DummyData {
     val data: List<MovieItem> = listOf(
         MovieItem(
-            "1",
+            1,
             "1",
             "1",
             "1"
         ),
         MovieItem(
-            "2",
+            2,
             "2",
             "2",
             "2"
         ),
         MovieItem(
-            "3",
+            3,
             "3",
             "3",
             "3"
         ),
         MovieItem(
-            "4",
+            4,
             "4",
             "4",
             "4"
         ),
         MovieItem(
-            "5",
+            5,
             "5",
             "5",
             "5"
