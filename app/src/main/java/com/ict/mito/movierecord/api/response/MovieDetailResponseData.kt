794fb9be45c9117771cfd99d0d6e0875ce1fb3dd
mitohato <mitohato14@gmail.com>
2019-07-02 15:00:53 +0900

    Implement qverride equals and hashcode method

--- app/src/main/java/com/ict/mito/movierecord/api/response/MovieDetailResponseData.kt
+++ app/src/main/java/com/ict/mito/movierecord/api/response/MovieDetailResponseData.kt
@@ -29,4 +29,41 @@ data class MovieDetailResponseData(
     val video: Boolean,
     val vote_average: Double,
     val vote_count: Int
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        return if (other is MovieDetailResponseData) {
+            id == other.id
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = adult.hashCode()
+        result = 31 * result + (backdrop_path?.hashCode() ?: 0)
+        result = 31 * result + (belongs_to_collection?.hashCode() ?: 0)
+        result = 31 * result + budget
+        result = 31 * result + genres.contentHashCode()
+        result = 31 * result + (homepage?.hashCode() ?: 0)
+        result = 31 * result + id
+        result = 31 * result + (imdb_id?.hashCode() ?: 0)
+        result = 31 * result + original_language.hashCode()
+        result = 31 * result + original_title.hashCode()
+        result = 31 * result + (overview?.hashCode() ?: 0)
+        result = 31 * result + popularity.hashCode()
+        result = 31 * result + (poster_path?.hashCode() ?: 0)
+        result = 31 * result + production_companies.contentHashCode()
+        result = 31 * result + production_countries.contentHashCode()
+        result = 31 * result + release_date.hashCode()
+        result = 31 * result + revenue
+        result = 31 * result + (runtime ?: 0)
+        result = 31 * result + spokenLanguages.contentHashCode()
+        result = 31 * result + status.hashCode()
+        result = 31 * result + (tagline?.hashCode() ?: 0)
+        result = 31 * result + title.hashCode()
+        result = 31 * result + video.hashCode()
+        result = 31 * result + vote_average.hashCode()
+        result = 31 * result + vote_count
+        return result
+    }
+}
--- app/src/main/java/com/ict/mito/movierecord/api/response/MovieNowPlayingListResponseData.kt
+++ app/src/main/java/com/ict/mito/movierecord/api/response/MovieNowPlayingListResponseData.kt
@@ -18,4 +18,30 @@ data class MovieNowPlayingListResponseData(
     val adult: Boolean,
     val overview: String,
     val release_date: String
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        return if (other is MovieDetailResponseData) {
+            id == other.id
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = vote_count
+        result = 31 * result + id
+        result = 31 * result + video.hashCode()
+        result = 31 * result + vote_averages.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + popularity.hashCode()
+        result = 31 * result + poster_path.hashCode()
+        result = 31 * result + original_language.hashCode()
+        result = 31 * result + original_title.hashCode()
+        result = 31 * result + genre_ids.contentHashCode()
+        result = 31 * result + backdrop_path.hashCode()
+        result = 31 * result + adult.hashCode()
+        result = 31 * result + overview.hashCode()
+        result = 31 * result + release_date.hashCode()
+        return result
+    }
+}
