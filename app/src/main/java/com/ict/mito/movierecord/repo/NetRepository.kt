98dae744acb8db3a9ac56ab75f8be8b3bbf0977e
mitohato <mitohato14@gmail.com>
2019-07-04 11:20:34 +0900

    Remove code for api client

--- app/src/main/java/com/ict/mito/movierecord/repo/NetRepository.kt
+++ app/src/main/java/com/ict/mito/movierecord/repo/NetRepository.kt
@@ -1,60 +1,19 @@
 package com.ict.mito.movierecord.repo
 
-import com.ict.mito.movierecord.BuildConfig
-import com.ict.mito.movierecord.api.MovieAPI
+import com.ict.mito.movierecord.api.ApiClient
 import com.ict.mito.movierecord.api.response.MovieDetailResponseData
 import com.ict.mito.movierecord.api.response.NowPlayingMovieList
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 
 /**
  * Created by mitohato14 on 2019-06-22.
  */
-class NetRepository {
-    private val baseUrl = BuildConfig.MOVIE_DB_BASE_URL
-    private val apiKey = BuildConfig.MOVIE_DB_API_KEY_V3
-    private var retrofit: Retrofit
-    private val service: MovieAPI
-    private val okHttpClient = OkHttpClient
-        .Builder()
-        .connectTimeout(
-            120,
-            TimeUnit.SECONDS
-        )
-        .readTimeout(
-            120,
-            TimeUnit.SECONDS
-        )
-        .addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        })
-        .build()
-
-    init {
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-
-        retrofit = Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .client(okHttpClient)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-
-        service = retrofit.create(MovieAPI::class.java)
-    }
-
+class NetRepository(private val apiClient: ApiClient) {
     fun getNowPlayingMovieList(): NowPlayingMovieList? {
         var list: NowPlayingMovieList? = null
         runBlocking {
-            val job = async { service.getNowPlayingMovieList(apiKey) }
+            val job = async { apiClient.service.getNowPlayingMovieList(apiClient.apiKey) }
             val res = job.await()
             if (res.isSuccessful) {
                 list = res.body()
@@ -67,9 +26,9 @@ class NetRepository {
         var responseData: MovieDetailResponseData? = null
         runBlocking {
             val job = async {
-                service.getMovieDetail(
+                apiClient.service.getMovieDetail(
                     movieId,
-                    apiKey
+                    apiClient.apiKey
                 )
             }
             val res = job.await()
