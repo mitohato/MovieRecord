8a574784214f3838a1c7ce2838055dd07b52e4f9
mitohato <mitohato14@gmail.com>
2019-08-11 20:27:17 +0900

    Extend RoomDataBase

--- app/src/main/java/com/ict/mito/movierecord/domain/db/MovieRoomDataBase.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/db/MovieRoomDataBase.kt
@@ -1,7 +1,12 @@
 package com.ict.mito.movierecord.domain.db
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ict.mito.movierecord.domain.MovieItem
+
 /**
  * Created by mitohato14 on 2019-08-11.
  */
-class MovieRoomDataBase {
+@Database(entities = [MovieItem::class], version = 1, exportSchema = false)
+abstract class MovieRoomDataBase : RoomDatabase() {
 }
