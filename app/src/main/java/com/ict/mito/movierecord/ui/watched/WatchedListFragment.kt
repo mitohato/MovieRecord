c9da68f09f2bff2e606b7da4a8b73aac9dc3d6ae
mitohato <mitohato14@gmail.com>
2019-08-11 19:09:09 +0900

    Delete unuse method

--- app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListFragment.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.ict.mito.movierecord.R
 import com.ict.mito.movierecord.databinding.WatchedListFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -48,11 +47,6 @@ class WatchedListFragment : Fragment() {
         return binding?.root
     }
 
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
-    }
 
     override fun onDestroy() {
         super.onDestroy()
