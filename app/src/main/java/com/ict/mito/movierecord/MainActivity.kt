db3e82253e56b6e84a8f73eb7fd1a9141ef6944d
mitohato <mitohato14@gmail.com>
2019-08-11 17:23:44 +0900

    Set toolbar

--- app/src/main/java/com/ict/mito/movierecord/MainActivity.kt
+++ app/src/main/java/com/ict/mito/movierecord/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.ict.mito.movierecord
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
 }
