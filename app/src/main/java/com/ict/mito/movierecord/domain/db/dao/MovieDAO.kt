9c6a38452d40183ff5aba5783b387d0017b09381
mitohato <mitohato14@gmail.com>
2019-08-11 20:51:58 +0900

    Add insert method

--- app/src/main/java/com/ict/mito/movierecord/domain/db/dao/MovieDAO.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/db/dao/MovieDAO.kt
@@ -1,10 +1,15 @@
 package com.ict.mito.movierecord.domain.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.ict.mito.movierecord.domain.MovieItem
 
 /**
  * Created by mitohato14 on 2019-08-11.
  */
 @Dao
 interface MovieDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(movieItem: MovieItem)
 }
