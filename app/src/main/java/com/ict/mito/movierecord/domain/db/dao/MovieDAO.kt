95969f9e9e3561a7c5804cea4a1eddd64651bef4
mitohato <mitohato14@gmail.com>
2019-08-11 20:54:31 +0900

    Add getAll method

--- app/src/main/java/com/ict/mito/movierecord/domain/db/dao/MovieDAO.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/db/dao/MovieDAO.kt
@@ -3,7 +3,9 @@ package com.ict.mito.movierecord.domain.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.ict.mito.movierecord.domain.MovieItem
+import io.reactivex.Single
 
 /**
  * Created by mitohato14 on 2019-08-11.
@@ -12,4 +14,7 @@ import com.ict.mito.movierecord.domain.MovieItem
 interface MovieDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(movieItem: MovieItem)
+
+    @Query("SELECT * FROM movierecord_movie_table")
+    fun getAllMovie(): Single<List<MovieItem>>
 }
