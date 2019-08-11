ef95b661678f3f060c06f66d125cb388dcbc3a24
mitohato <mitohato14@gmail.com>
2019-08-11 19:05:33 +0900

    Set viewmodel to module

--- app/src/main/java/com/ict/mito/movierecord/App.kt
+++ app/src/main/java/com/ict/mito/movierecord/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.ict.mito.movierecord.api.ApiClient
 import com.ict.mito.movierecord.repo.NetRepository
 import com.ict.mito.movierecord.ui.top.TopViewModel
+import com.ict.mito.movierecord.ui.watched.WatchedListViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -34,6 +35,7 @@ class App : Application() {
 
     private val viewModelModule: Module = module {
         viewModel { TopViewModel(get()) }
+        viewModel { WatchedListViewModel() }
     }
 
     private val repositoryModule: Module = module {
