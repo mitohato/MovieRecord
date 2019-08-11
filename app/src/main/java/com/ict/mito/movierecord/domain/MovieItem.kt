870e26ad3d77922c1633cd364d2a5a5085c5a9e0
mitohato <mitohato14@gmail.com>
2019-08-11 20:36:32 +0900

    Add entity annotation

--- app/src/main/java/com/ict/mito/movierecord/domain/MovieItem.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/MovieItem.kt
@@ -1,8 +1,11 @@
 package com.ict.mito.movierecord.domain
 
+import androidx.room.Entity
+
 /**
  * Created by mitohato14 on 2019/04/08.
  */
+@Entity(tableName = "movierecord_movie_table")
 data class MovieItem(
     val id: Int,
     val title: String,
