8342b150bbff4b868254925f95f1325d7c901aed
mitohato <mitohato14@gmail.com>
2019-08-11 18:40:34 +0900

    Implement get layout id method

--- app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListRowItem.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListRowItem.kt
@@ -1,5 +1,6 @@
 package com.ict.mito.movierecord.ui.watched
 
+import com.ict.mito.movierecord.R
 import com.ict.mito.movierecord.databinding.MovieCardBinding
 import com.ict.mito.movierecord.domain.MovieItem
 import com.xwray.groupie.databinding.BindableItem
@@ -8,9 +9,7 @@ import com.xwray.groupie.databinding.BindableItem
  * Created by mitohato14 on 2019-08-11.
  */
 class WatchedListRowItem(val movieItem: MovieItem): BindableItem<MovieCardBinding>() {
-    override fun getLayout(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getLayout(): Int = R.layout.movie_card
 
     override fun bind(viewBinding: MovieCardBinding, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
