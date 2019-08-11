1d335e61b6e01de284246d71fc1953d33e6972aa
mitohato <mitohato14@gmail.com>
2019-08-11 20:31:26 +0900

    Add singletoon DataBase var

--- app/src/main/java/com/ict/mito/movierecord/domain/db/MovieRoomDataBase.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/db/MovieRoomDataBase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.ict.mito.movierecord.domain.MovieItem
 import com.ict.mito.movierecord.domain.db.dao.MovieDAO
+import javax.inject.Singleton
 
 /**
  * Created by mitohato14 on 2019-08-11.
@@ -17,6 +18,8 @@ abstract class MovieRoomDataBase : RoomDatabase() {
     abstract fun movieDao(): MovieDAO
 
     companion object {
+        @Singleton
+        private var INSTANCE: MovieRoomDataBase? = null
         
 
     }
