60d768a8360b840d8450957f05a3ced910599f79
mitohato <mitohato14@gmail.com>
2019-08-11 20:29:15 +0900

    Devide annotation args

--- app/src/main/java/com/ict/mito/movierecord/domain/db/MovieRoomDataBase.kt
+++ app/src/main/java/com/ict/mito/movierecord/domain/db/MovieRoomDataBase.kt
@@ -7,6 +7,10 @@ import com.ict.mito.movierecord.domain.MovieItem
 /**
  * Created by mitohato14 on 2019-08-11.
  */
-@Database(entities = [MovieItem::class], version = 1, exportSchema = false)
+@Database(
+    entities = [MovieItem::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class MovieRoomDataBase : RoomDatabase() {
 }
