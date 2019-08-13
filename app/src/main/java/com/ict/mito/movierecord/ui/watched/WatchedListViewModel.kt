178328bff8e0a424b99425a38c20632a8dd978ed
mitohato <mitohato14@gmail.com>
2019-08-14 00:03:23 +0900

    Add private var for coroutine

--- app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListViewModel.kt
@@ -5,8 +5,17 @@ import androidx.lifecycle.ViewModel
 import com.ict.mito.movierecord.repo.Repository
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.databinding.ViewHolder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 
 class WatchedListViewModel(private val repository: Repository) : ViewModel() {
     val bindableRowItemList: MutableLiveData<ArrayList<WatchedListRowItem>> = MutableLiveData()
     val groupAdapter: GroupAdapter<ViewHolder<*>> = GroupAdapter()
+
+    private val parentJob = Job()
+    private val mainCoroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+    private val scope = CoroutineScope(mainCoroutineContext)
 }
