d10da6d3ba3d03a64bf1e3a240d824b419f98a1c
mitohato <mitohato14@gmail.com>
2019-08-14 00:01:33 +0900

    Add repo constructor

--- app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListViewModel.kt
@@ -2,10 +2,11 @@ package com.ict.mito.movierecord.ui.watched
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ict.mito.movierecord.repo.Repository
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.databinding.ViewHolder
 
-class WatchedListViewModel : ViewModel() {
+class WatchedListViewModel(private val repository: Repository) : ViewModel() {
     val bindableRowItemList: MutableLiveData<ArrayList<WatchedListRowItem>> = MutableLiveData()
     val groupAdapter: GroupAdapter<ViewHolder<*>> = GroupAdapter()
 }
