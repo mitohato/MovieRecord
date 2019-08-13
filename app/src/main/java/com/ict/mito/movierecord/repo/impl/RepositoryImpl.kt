21235a5c9d34f56f9bf5cea9d287a3c07a50af70
mitohato <mitohato14@gmail.com>
2019-08-13 23:26:05 +0900

    Add constructorfor dao

--- app/src/main/java/com/ict/mito/movierecord/repo/impl/RepositoryImpl.kt
+++ app/src/main/java/com/ict/mito/movierecord/repo/impl/RepositoryImpl.kt
@@ -1,13 +1,14 @@
 package com.ict.mito.movierecord.repo.impl
 
 import com.ict.mito.movierecord.domain.MovieItem
+import com.ict.mito.movierecord.domain.db.dao.MovieDAO
 import com.ict.mito.movierecord.repo.Repository
 import io.reactivex.Single
 
 /**
  * Created by mitohato14 on 2019-08-13.
  */
-class RepositoryImpl : Repository {
+class RepositoryImpl(private val dao: MovieDAO) : Repository {
     override fun getAllMovie(): Single<List<MovieItem>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
