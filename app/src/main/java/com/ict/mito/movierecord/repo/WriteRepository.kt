c5ea51ac37e3633537cdd054336adaf1f114a892
mitohato <mitohato14@gmail.com>
2019-08-13 23:21:22 +0900

    Add method in write repo

--- app/src/main/java/com/ict/mito/movierecord/repo/WriteRepository.kt
+++ app/src/main/java/com/ict/mito/movierecord/repo/WriteRepository.kt
@@ -1,7 +1,10 @@
 package com.ict.mito.movierecord.repo
 
+import com.ict.mito.movierecord.domain.MovieItem
+
 /**
  * Created by mitohato14 on 2019-08-13.
  */
 interface WriteRepository {
+    fun add(movieItem: MovieItem)
 }
