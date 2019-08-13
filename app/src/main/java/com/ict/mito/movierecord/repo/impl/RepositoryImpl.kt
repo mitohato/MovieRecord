f5b1fd3b684ac5ca3591f9ea6a6f7c9e277bb239
mitohato <mitohato14@gmail.com>
2019-08-13 23:58:07 +0900

    Add workerthred annotation

--- app/src/main/java/com/ict/mito/movierecord/repo/impl/RepositoryImpl.kt
+++ app/src/main/java/com/ict/mito/movierecord/repo/impl/RepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.ict.mito.movierecord.repo.impl
 
+import androidx.annotation.WorkerThread
 import com.ict.mito.movierecord.domain.MovieItem
 import com.ict.mito.movierecord.domain.db.dao.MovieDAO
 import com.ict.mito.movierecord.repo.Repository
@@ -9,7 +10,9 @@ import io.reactivex.Single
  * Created by mitohato14 on 2019-08-13.
  */
 class RepositoryImpl(private val dao: MovieDAO) : Repository {
+    @WorkerThread
     override fun getAllMovie(): Single<List<MovieItem>> = dao.getAllMovie()
 
+    @WorkerThread
     override fun add(movieItem: MovieItem) = dao.insert(movieItem)
 }
