0992bdff83ae3fa375a5a762fb9b45c268ebc78e
mitohato <mitohato14@gmail.com>
2019-08-13 23:25:15 +0900

    add and extend repo

---
+++ app/src/main/java/com/ict/mito/movierecord/repo/impl/RepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.ict.mito.movierecord.repo.impl
+
+import com.ict.mito.movierecord.domain.MovieItem
+import com.ict.mito.movierecord.repo.Repository
+import io.reactivex.Single
+
+/**
+ * Created by mitohato14 on 2019-08-13.
+ */
+class RepositoryImpl : Repository {
+    override fun getAllMovie(): Single<List<MovieItem>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun add(movieItem: MovieItem) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
