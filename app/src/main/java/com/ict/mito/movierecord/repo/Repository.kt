9bdb5c3a94fbee6ffe9b480c0ea9bafa3fb76c04
mitohato <mitohato14@gmail.com>
2019-08-13 23:22:26 +0900

    Remove {}

--- app/src/main/java/com/ict/mito/movierecord/repo/Repository.kt
+++ app/src/main/java/com/ict/mito/movierecord/repo/Repository.kt
@@ -3,5 +3,4 @@ package com.ict.mito.movierecord.repo
 /**
  * Created by mitohato14 on 2019-08-13.
  */
-interface Repository : ReadRepository, WriteRepository {
-}
+interface Repository : ReadRepository, WriteRepository
