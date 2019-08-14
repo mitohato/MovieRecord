1d0c13a83d769d252dcb9e86e183d228aee56ca4
mitohato <mitohato14@gmail.com>
2019-08-14 14:00:56 +0900

    Fix runtime exception

--- app/src/main/java/com/ict/mito/movierecord/api/response/MovieNowPlayingListResponseData.kt
+++ app/src/main/java/com/ict/mito/movierecord/api/response/MovieNowPlayingListResponseData.kt
@@ -12,11 +12,11 @@ data class MovieNowPlayingListResponseData(
     @Json(name = "vote_average") val voteAverage: Double,
     @Json(name = "title") val title: String,
     @Json(name = "popularity") val popularity: Double,
-    @Json(name = "poster_path") val posterPath: String,
+    @Json(name = "poster_path") val posterPath: String?,
     @Json(name = "original_language") val originalLanguage: String,
     @Json(name = "original_title") val originalTitle: String,
     @Json(name = "genre_ids") val genreIds: Array<Int>,
-    @Json(name = "backdrop_path") val backdropPath: String,
+    @Json(name = "backdrop_path") val backdropPath: String?,
     @Json(name = "adult") val adult: Boolean,
     @Json(name = "overview") val overview: String,
     @Json(name = "release_date") val releaseDate: String
