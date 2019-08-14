97c34e3c1f6a0286cebdf376368604993105ee64
mitohato <mitohato14@gmail.com>
2019-08-14 10:39:39 +0900

    Add init logic

--- app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/watched/WatchedListViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.ViewModel
 import com.ict.mito.movierecord.repo.Repository
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.databinding.ViewHolder
+import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class WatchedListViewModel(private val repository: Repository) : ViewModel() {
@@ -18,4 +20,21 @@ class WatchedListViewModel(private val repository: Repository) : ViewModel() {
     private val mainCoroutineContext: CoroutineContext
         get() = parentJob + Dispatchers.Main
     private val scope = CoroutineScope(mainCoroutineContext)
+
+    init {
+        scope.launch(Dispatchers.IO) {
+            repository.getAllMovie().subscribeBy(
+                onSuccess = {
+                    val list = arrayListOf<WatchedListRowItem>()
+                    it.forEach { item ->
+                        list.add(WatchedListRowItem(item))
+                    }
+                    bindableRowItemList.postValue(list)
+                },
+                onError = {
+                    throw it
+                }
+            )
+        }
+    }
 }
