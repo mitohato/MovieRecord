aeb4326b2c873190657fa81502a023fe66b9d362
mitohato <mitohato14@gmail.com>
2019-08-18 18:31:00 +0900

    Add repository constructor

--- app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
@@ -2,8 +2,9 @@ package com.ict.mito.movierecord.ui.detail
 
 import androidx.lifecycle.ViewModel
 import com.ict.mito.movierecord.domain.DetailMovieItem
+import com.ict.mito.movierecord.repo.NetRepository
 
-class DetailViewModel : ViewModel() {
+class DetailViewModel(private val repository: NetRepository) : ViewModel() {
     lateinit var detailMovieItem: DetailMovieItem
     var movieId: Int = -1
 }
