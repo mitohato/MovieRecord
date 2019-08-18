33a71d42f36a76e342495e5bbfdc1b2c91cfb324
mitohato <mitohato14@gmail.com>
2019-08-18 17:55:08 +0900

    Add navConrtoller

--- app/src/main/java/com/ict/mito/movierecord/ui/top/TopRowItem.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/top/TopRowItem.kt
@@ -1,5 +1,6 @@
 package com.ict.mito.movierecord.ui.top
 
+import androidx.navigation.NavController
 import com.ict.mito.movierecord.R
 import com.ict.mito.movierecord.databinding.MovieCardBinding
 import com.ict.mito.movierecord.domain.MovieItem
@@ -8,7 +9,10 @@ import com.xwray.groupie.databinding.BindableItem
 /**
  * Created by mitohato14 on 2019-06-19.
  */
-class TopRowItem(val movieItem: MovieItem) : BindableItem<MovieCardBinding>() {
+class TopRowItem(
+    val movieItem: MovieItem,
+    val navController: NavController
+) : BindableItem<MovieCardBinding>() {
     override fun getLayout(): Int = R.layout.movie_card
 
     override fun bind(
