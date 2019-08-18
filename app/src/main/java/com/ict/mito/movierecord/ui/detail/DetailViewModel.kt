e8fc83d226de617722c06f33eb0368ab055d96c3
mitohato <mitohato14@gmail.com>
2019-08-18 18:27:28 +0900

    Add detail item

--- app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
@@ -1,7 +1,9 @@
 package com.ict.mito.movierecord.ui.detail
 
 import androidx.lifecycle.ViewModel
+import com.ict.mito.movierecord.domain.DetailMovieItem
 
 class DetailViewModel : ViewModel() {
+    lateinit var detailMovieItem: DetailMovieItem
     var movieId: Int = -1
 }
