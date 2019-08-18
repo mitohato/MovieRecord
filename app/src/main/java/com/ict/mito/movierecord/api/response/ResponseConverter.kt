1bceff7596e99c35efe8a2ac89b23a7aa445d073
mitohato <mitohato14@gmail.com>
2019-08-18 18:37:49 +0900

    Add convertor method

--- app/src/main/java/com/ict/mito/movierecord/api/response/ResponseConverter.kt
+++ app/src/main/java/com/ict/mito/movierecord/api/response/ResponseConverter.kt
@@ -1,6 +1,7 @@
 package com.ict.mito.movierecord.api.response
 
 import com.ict.mito.movierecord.BuildConfig
+import com.ict.mito.movierecord.domain.DetailMovieItem
 import com.ict.mito.movierecord.domain.MovieItem
 
 /**
@@ -14,6 +15,23 @@ class ResponseConverter {
             description = response.overview,
             id = response.id,
             imageUrl = imageBaseUrl + response.posterPath
+        )
+    }
+
+    fun convertToDetailMovieItemFromMovieDetail(response: MovieDetailResponseData): DetailMovieItem {
+        val imageBaseUrl: String = BuildConfig.MOVIE_DB_IMAGE_BASE_URL
+        return DetailMovieItem(
+            movieId = response.id,
+            genres = response.genres,
+            homepageUrl = response.homepage ?: "",
+            originalLanguage = response.originalLanguage,
+            originalTitle = response.originalTitle,
+            overview = response.overview ?: "",
+            popularity = response.popularity,
+            posterPath = imageBaseUrl + response.posterPath,
+            releaseDate = response.releaseDate,
+            spokenLanguage = response.spokenLanguages,
+            title = response.title
         )
     }
 }
