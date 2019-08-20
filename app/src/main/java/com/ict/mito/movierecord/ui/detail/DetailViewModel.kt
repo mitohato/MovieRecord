0de40dc8bf787d06c0e8932bae4f46e42b44d85f
mitohato <mitohato14@gmail.com>
2019-08-20 12:31:02 +0900

    Rename repository from net

--- app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
@@ -5,12 +5,14 @@ import com.ict.mito.movierecord.api.response.ResponseConverter
 import com.ict.mito.movierecord.domain.DetailMovieItem
 import com.ict.mito.movierecord.repo.NetRepository
 
-class DetailViewModel(private val repository: NetRepository) : ViewModel() {
+class DetailViewModel(
+    private val netRepository: NetRepository,
+) : ViewModel() {
     lateinit var detailMovieItem: DetailMovieItem
     var movieId: Int = -1
         set(value) {
             field = value
-            repository.getMovieDetail(value)?.let {
+            netRepository.getMovieDetail(value)?.let {
                 detailMovieItem = ResponseConverter().convertToDetailMovieItemFromMovieDetail(it)
             }
         }
