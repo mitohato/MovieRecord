1b4cf903a037eb3e20498157694b58244a357506
mitohato <mitohato14@gmail.com>
2019-08-20 12:31:24 +0900

    Add repository from room

--- app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
@@ -1,12 +1,16 @@
 package com.ict.mito.movierecord.ui.detail
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 import com.ict.mito.movierecord.api.response.ResponseConverter
 import com.ict.mito.movierecord.domain.DetailMovieItem
+import com.ict.mito.movierecord.domain.MovieItem
 import com.ict.mito.movierecord.repo.NetRepository
+import com.ict.mito.movierecord.repo.Repository
 
 class DetailViewModel(
     private val netRepository: NetRepository,
+    private val repository: Repository
 ) : ViewModel() {
     lateinit var detailMovieItem: DetailMovieItem
     var movieId: Int = -1
