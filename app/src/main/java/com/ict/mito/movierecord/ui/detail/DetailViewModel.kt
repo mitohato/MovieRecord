4e661102b2d1f681425d55ba3b8e578a22f2b972
mitohato <mitohato14@gmail.com>
2019-08-22 07:48:41 +0900

    Make MutableLiveData

--- app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/ict/mito/movierecord/ui/detail/DetailViewModel.kt
@@ -1,6 +1,7 @@
 package com.ict.mito.movierecord.ui.detail
 
 import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ict.mito.movierecord.api.response.ResponseConverter
 import com.ict.mito.movierecord.domain.DetailMovieItem
@@ -12,24 +13,28 @@ class DetailViewModel(
     private val netRepository: NetRepository,
     private val repository: Repository
 ) : ViewModel() {
-    lateinit var detailMovieItem: DetailMovieItem
+    lateinit var detailMovieItem: MutableLiveData<DetailMovieItem>
     var movieId: Int = -1
         set(value) {
             field = value
             netRepository.getMovieDetail(value)?.let {
-                detailMovieItem = ResponseConverter().convertToDetailMovieItemFromMovieDetail(it)
+                detailMovieItem.postValue(
+                    ResponseConverter().convertToDetailMovieItemFromMovieDetail(it)
+                )
             }
         }
 
     fun addToWatchedList(view: View) {
-        repository.add(
-            MovieItem(
-                id = detailMovieItem.movieId,
-                title = detailMovieItem.title,
-                description = detailMovieItem.overview,
-                imageUrl = detailMovieItem.posterPath,
-                watched = true
+        detailMovieItem.value.also {
+            repository.add(
+                MovieItem(
+                    id = it.movieId,
+                    title = it.title,
+                    description = it.overview,
+                    imageUrl = it.posterPath,
+                    watched = it.watched
+                )
             )
-        )
+        }
     }
 }
