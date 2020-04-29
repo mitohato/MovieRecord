4c5f3da9ac33e6ecfb811ab979d404c2b69ae16b
mito <mitohato@users.noreply.github.com>
2020-04-29 16:19:25 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,13 @@ buildscript {
     repositories {
         google()
         jcenter()
+        maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:8.2.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
